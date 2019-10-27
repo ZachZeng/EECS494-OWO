4d6db15ca377384ecd1ab13431a717499583db4c
chenjq <chenjq@umich.edu>
2019-10-26 22:42:14 -0400

    add Enemy Pathfinding, Enemy Generating

---
+++ Assets/Scripts/TargetControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    Vector3 destination;
+    void Start()
+    {
+        destination = new Vector3(transform.position.x, transform.position.y, 3.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+    }
+}
