ebca9c5ef0627e5da469a224cfdc00990ec0c2e4
chenjq <chenjq@umich.edu>
2019-10-26 23:41:39 -0400

    add enemy health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    int maxHealth = 100;
+    int currentHealth;
+    public event Action<float> onHealthChange = delegate { };
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+    void ModifyHealth(int amount)
+    {
+        currentHealth += amount;
+        float currentHealthPct = (float)currentHealth / (float)maxHealth;
+        onHealthChange(currentHealthPct);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ModifyHealth(-10);
+        }
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
