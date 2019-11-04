e43b3b31581f479ba26a0463c4601445284be4c5
guogerik <guogerik@umich.edu>
2019-11-03 22:27:45 -0500

    added fader between scenes in Fader

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    private int levelToLoad = 1;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameController.instance.playerChosen[0] != -1 && GameController.instance.playerChosen[1] != -1)
+        {
+            FadeToLevel(1);
+        }
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+        levelToLoad = levelIndex;
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
